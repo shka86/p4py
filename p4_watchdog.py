428d4546c0a600b07031e8e445f9542482808f28
shka <45268944+shka86@users.noreply.github.com>
2020-05-22 21:13:41 +0900

    Update p4_watchdog.py

--- p4_watchdog.py
+++ p4_watchdog.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-# import textwrap
-# import shutil
 import subprocess
-# import json
-# from pathlib import Path as p
-# from distutils import dir_util as du
 import datetime
-# from pprint import pprint as pp
 import time
 
 # ##################################
